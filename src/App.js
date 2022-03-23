da939e7526a5460b486b93bf251cd4ee806abe4b
alvinle29 <bach938@gmail.com>
2022-03-23 17:33:40 +0200

    storage adding

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { GiHornedHelm } from 'react-icons/gi'
+import { AiOutlinePlus, AiOutlineClose } from 'react-icons/ai'
 
 function App() {
+  const [tasks, setTasks] = useState([])
+  const [input, setInput] = useState('')
+
+  const date = new Date()
+  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+  const saveData = (newTasks) => {
+    localStorage.setItem("tasks", JSON.stringify(newTasks));
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem("tasks")){
+      setTasks(JSON.parse(localStorage.getItem("tasks")));
+    }
+  }, []);
+
+  //submit + add tasks
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const addTask = {
+      id: Math.floor(Math.random() * 1000),
+      text: input,
+      completed: false
+    }
+    setTasks([...tasks, addTask])
+    setInput('')
+    saveData([...tasks, addTask])
+  }
+
+  //delete tasks
+  const deleteTask = (id) => {
+    let filteredTasks = [...tasks].filter((tasks) => tasks.id !== id)
+    setTasks(filteredTasks)
+    saveData(filteredTasks)
+  }
+
+  //completed task
+  const toggleComplete = (id) => {
+    setTasks(
+      tasks.map(task => (
+        task.id === id ? { ...task, completed: !task.completed } : task
+      ))
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className="container">
+        <h1><GiHornedHelm /> My todo list</h1>
+
+        <div className="date">
+          <p>{days[date.getDay()]}</p>
+          <p>{date.getDate()},</p>
+          <p>{months[date.getMonth()]}</p>
+          <p>{date.getFullYear()}</p>
+        </div>
+
+        <form onSubmit={handleSubmit}>
+          <div className="form-input">
+            <AiOutlinePlus className='icon' />
+            <input
+              value={input}
+              onChange={e => setInput(e.target.value)}
+              placeholder='Enter a task'
+              type="text" />
+          </div>
+        </form>
+
+        <div>
+          {tasks.map(task => (
+            <div className={`task-row ${task.completed ? 'completed' : ''}`} key={task.id} onDoubleClick={() => toggleComplete(task.id)} >
+              <p>{task.text} </p>
+              <AiOutlineClose onClick={() => deleteTask(task.id)} className='icon' />
+            </div>
+          ))}
+        </div>
+
+          <p className='length'>{(tasks < 1) ? 'You have no tasks' : `Tasks: ${tasks.length}`}</p>
+      </div>
     </div>
   );
 }
-
 export default App;
